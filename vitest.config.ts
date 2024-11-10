6576bdece2dcff4087b2d35e04a091239764811a
letbueno <leticia.bueno.martins@gmail.com>
2024-11-10 20:43:07 -0300

    feat: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./src/setupTests.ts"],
+    css: true,
+
+    coverage: {
+      exclude: ["src/components/ui", "src/utils", "src/types"],
+    },
+  },
+});
