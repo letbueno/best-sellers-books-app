78c98e209075618bee1bdbc151c726f9a2197358
letbueno <leticia.bueno.martins@gmail.com>
2024-11-12 21:56:51 -0300

    fix: remove console log and unnecessary component

--- src/services/api/bestSellersApi/index.ts
+++ src/services/api/bestSellersApi/index.ts
@@ -1,8 +1,14 @@
 import { AxiosResponse } from "axios";
 import { apiGet } from "../index";
+import { ListBooks } from "../../../types/ListBooks";
 
+type ApiListBooks = {
+  results: {
+    lists: ListBooks[];
+  };
+};
 const bestSellersBooksApi = {
-  getbestSellersBooks: (): Promise<AxiosResponse<any>> =>
+  getbestSellersBooks: (): Promise<AxiosResponse<ApiListBooks>> =>
     apiGet("full-overview.json"),
 };
 
--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -3,13 +3,11 @@ import Axios from "axios";
 export const baseURL = "https://api.nytimes.com/svc/books/v3/lists/";
 export const API_KEY = process.env.REACT_APP_API_KEY;
 
-console.log("API_KEY", API_KEY);
 const api = Axios.create({
   baseURL,
   headers: {
     "Content-Type": "application/json",
   },
-  validateStatus: (status) => status >= 200 && status < 300,
 });
 
 export function apiGet(url: string) {
